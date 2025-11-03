dd7383077bb02d8bac106800c5507cdf5700f338
Park-JiYeong <89844427+ParkJiYeoung8297@users.noreply.github.com>
2025-11-04 01:52:47 +0900

    [level 3] Title: 입국심사, Time: 517.93 ms, Memory: 13.4 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -2,7 +2,7 @@ def solution(n, times):
     answer=float('inf')
     min_time=min(times)
     start=min_time
-    end=min_time*n
+    end=min_time*n # 이렇게 안하면 시간 초과남 (젤 적게 걸리는 애 혼자 다 처리하는게 최대)
     
     while (1):
         if end==start:
@@ -13,10 +13,9 @@ def solution(n, times):
             count+=mid//i
         # print(start, end, count)
         if count>=n:
-            answer=mid
+            answer=mid # 이거 갱신 부분(내가 잘 놓치는 파트) → 조건 만족하면 값 갱신
             end=mid # mid도 포함
         else:
             start=mid+1 # mid 포함 안함
         
-    
     return answer
