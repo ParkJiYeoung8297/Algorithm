fa24b5a08ed638573864f74d6b05c89cff3c95b4
Park-JiYeong <89844427+ParkJiYeoung8297@users.noreply.github.com>
2025-11-19 10:51:07 +0900

    [level 3] Title: 이중우선순위큐, Time: 280.90 ms, Memory: 15.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,25 +1,15 @@
 import heapq
 def solution(operations):
     answer = []
-    queue=[]
-
-
-    for oper in operations:
-        op=oper.split(" ")
-        if op[0]=="I":
-            heapq.heappush(queue,int(op[1]))
-        if queue:
-            if op[0]=="D" and op[1]=="-1":
-                heapq.heappop(queue)
-            if op[0]=="D" and op[1]=="1":
-                queue.remove(max(queue))
-
-    # 마지막 정렬 필요 (heap은 최솟갑을 빠르게 찾는거지, 정렬이 되어있는게 아님)
-    queue.sort()
-    if len(queue)==0:
-        answer=[0,0]
-    elif len(queue)==1:
-        answer=[queue[0],queue[0]]
-    elif len(queue)>=2:
-        answer=[queue[-1],queue[0]]
-    return answer
+    heap=[]
+    for op in operations:
+        A=op.split(" ")
+        if A[0]=="I":
+            heapq.heappush(heap,int(A[1]))
+        elif A[0]=="D" and heap:
+            if A[1]=="1":
+                value=max(heap)
+                heap.remove(value)
+            elif A[1]=="-1":
+                heapq.heappop(heap)
+    return [max(heap),heapq.heappop(heap)] if heap else [0,0]
