04cb662a3a8b6b6108ca5823b992242cd75141d7
Park-JiYeong <89844427+ParkJiYeoung8297@users.noreply.github.com>
2025-11-21 00:50:49 +0900

    [level 3] Title: 등굣길, Time: 3.44 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,25 +1,20 @@
 def solution(m, n, puddles):
-    board=[[0]*(m+1) for _ in range(n+1)]
-    for y in range(1,n+1):
-        board[y][1]=1
-    for x in range(1,m+1):
-        board[1][x]=1
+    answer = 0
+    # 오른쪽 또는 아래로만 움직임
+    dp=[[0]*(m+1) for _ in range(n+1)]
     
-    for px,py in puddles:
-        board[py][px]=-1
-        
-    # 오른쪽, 아래로만 이동 가능 = 왼쪽/위의 값이 연못이 아니면 더함
-    for cy in range(1,n+1):
-        for cx in range(1,m+1):
-            if cy==1 and cx==1:
-                continue
-            if 1<=cy<=n and 1<=cx<=m and board[cy-1][cx]!=-1 and board[cy][cx]!=-1 and board[cy][cx-1]!=-1:
-                board[cy][cx]=board[cy-1][cx]+board[cy][cx-1]
+    dp[1][1]=1
+    
+    # 물 웅덩이 표시
+    for x,y in puddles:
+        dp[y][x]=-1
 
-            elif 1<=cy<=n and board[cy-1][cx]!=-1 and board[cy][cx]!=-1:
-                board[cy][cx]=board[cy-1][cx]
+    for y in range(1,n+1):
+        for x in range(1,m+1):
+            if dp[y][x]!=-1:
+                if dp[y][x-1]!=-1:
+                    dp[y][x]+=dp[y][x-1]
+                if dp[y-1][x]!=-1:
+                    dp[y][x]+=dp[y-1][x]
 
-            elif 1<=cx<=m and board[cy][cx-1]!=-1 and board[cy][cx]!=-1:
-                board[cy][cx]=board[cy][cx-1]
-
-    return (board[n][m])%1000000007 # 칸 수여서 마지막+1은 뻄
+    return dp[n][m]%1000000007
