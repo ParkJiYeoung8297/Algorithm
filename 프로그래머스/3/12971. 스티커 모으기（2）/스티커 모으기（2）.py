45aa9ae341bfeb409b13aa4dd1e4e66212873fb3
Park-JiYeong <89844427+ParkJiYeoung8297@users.noreply.github.com>
2025-11-25 01:25:54 +0900

    [level 3] Title: 스티커 모으기(2), Time: 65.47 ms, Memory: 15.8 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -1,26 +1,21 @@
-
 def solution(sticker):
     answer = 0
+    dp_first=[0]*len(sticker)
+    dp_second=[0]*len(sticker)
+    if len(sticker)==1:
+        return sticker[0]
+    
 
-    if len(sticker)<=3:
-        return max(sticker)
-    
-    # 스티커 뗼건지, 말건지 선택(dfs 말고 dp로 선택하기)
-    dp=[0]*(len(sticker)-1)
-    
-    #0을 똈을 때
-    dp=[0]*(len(sticker)-1)
-    dp[0]=sticker[0]
-    dp[1]=max(sticker[1],dp[0])  # 이거 유의하기! dp[1]도 채워줘야함
+    # 첫번쨰 스티커를 떼는 경우
+    dp_first[0]=sticker[0]
+    dp_first[1]=max(sticker[1],dp_first[0])  # 이거 유의하기! dp[1]도 채워줘야함
     for i in range(2,len(sticker)-1):
-        dp[i]=max(dp[i-1],dp[i-2]+sticker[i]) # 안떼기, 떼기
-    answer=dp[-1]
-    # 0 안뗐을 때
-    dp=[0]*len(sticker)
-    dp[1]=sticker[1]
-    for i in range(2,len(sticker)):
-        dp[i]=max(dp[i-1],dp[i-2]+sticker[i]) # 안떼기, 떼기
-    answer=max(answer,dp[-1])
-    return answer
-
+        dp_first[i]=max(dp_first[i-2]+sticker[i], dp_first[i-1])
+        
     
+    # 첫번째 스티커를 안떼는 경우
+    dp_second[1]=sticker[1]
+    for i in range(2,len(dp_second)):
+        dp_second[i]=max(dp_second[i-2]+sticker[i], dp_second[i-1])
+
+    return max(max(dp_first),max(dp_second))
