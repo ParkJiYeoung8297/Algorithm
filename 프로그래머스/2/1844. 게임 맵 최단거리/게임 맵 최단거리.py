8ef888d6f7e2c1102cf53d0b40fbd486f8eb030f
Park-JiYeong <89844427+ParkJiYeoung8297@users.noreply.github.com>
2025-12-02 02:25:04 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 15.66 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -9,16 +9,16 @@ def solution(maps):
         queue.append((y,x,c)) # y,x,cost
         while queue:
             cy,cx,cost=queue.popleft()
-            visited[cy][cx]=1
             if cy==len(maps)-1 and cx==len(maps[0])-1:
                 return cost
             
             for dir in direction:
                 ny=cy+dir[0]
                 nx=cx+dir[1]
-                if 0<=ny<len(maps) and 0<=nx<len(maps[0]) and maps[ny][nx]==1 and visited[ny][nx]==0:
-                    queue.append((ny,nx,cost+1))
-                    visited[ny][nx]=1
+                if 0<=ny<len(maps) and 0<=nx<len(maps[0]):
+                    if maps[ny][nx]==1 and visited[ny][nx]==0:
+                        queue.append((ny,nx,cost+1))
+                        visited[ny][nx]=1
         return -1
                 
     return bfs(0,0,1)  
